bc9d0775605a7ca0627b0ea61fc75ef385372a9c
mkf <mkf@cloud9p.org>
2024-10-09 12:08:39 +0000

    src/main.c: add return values and fix style

--- src/main.c
+++ src/main.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "main.h"
 
-static void print_usage(char const* app)
+static void
+usage(char const* app)
 {
-    fprintf(stderr, "uasge: %s [fics | add-player | make-rank] [options]\n", app);
+	fprintf(stderr, "uasge: %s [fics | add-player | make-rank] [options]\n", app);
+	exit(1);
 }
 
-int main(int argc, char** argv)
+int
+main(int argc, char **argv)
 {
-    if(argc < 2)
-    {
-        print_usage(argv[0]);
-        return 1;
-    }
+	if(argc < 2)
+		usage(argv[0]);
 
-    if(strcmp(argv[1], "fics") == 0)
-        fics_main(argc - 1, argv + 1);
-    else if(strcmp(argv[1], "add-player") == 0)
-        add_player_main(argc - 1, argv + 1);
-    else if(strcmp(argv[1], "make-rank") == 0)
-        make_rank_main(argc - 1, argv + 1);
+	if(strcmp(argv[1], "fics") == 0)
+		fics_main(argc - 1, argv + 1);
+	else if(strcmp(argv[1], "add-player") == 0)
+		add_player_main(argc - 1, argv + 1);
+	else if(strcmp(argv[1], "make-rank") == 0)
+		make_rank_main(argc - 1, argv + 1);
+	else
+		usage(argv[0]);
 
-    else
-        print_usage(argv[0]);
+	return 0;
 }
